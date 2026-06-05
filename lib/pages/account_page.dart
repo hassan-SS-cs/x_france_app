3065655aa4e63d77546de8cea20c934b4d81974a
hassan-SS-cs <has597962@gmail.com>
2026-06-05 08:22:39 +0300

    Refactor AccountPage UI to enhance user experience; implement login functionality and improve input fields design

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,56 +1,146 @@
 import 'package:flutter/material.dart';
-import 'package:x_french/widgets/Submit_Button.dart';
-import 'package:x_french/widgets/text_field.dart';
 
 class AccountPage extends StatefulWidget {
-AccountPage({super.key});
+  AccountPage({super.key});
 
-  final emailController =  TextEditingController();
-  final passwordController =  TextEditingController();
-
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   State<AccountPage> createState() => _AccountPageState();
 }
 
 class _AccountPageState extends State<AccountPage> {
+  bool isLoggedIn = false;
+
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: 500
-            ),
-            child: Column(
-              children: [
-                Spacer(),
-                Text('Sign in/up',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),),
-              SizedBox(height: 30,),
-              InputTextField(
-                controller: widget.emailController,
-                hintText: "email@.com",
-                obscureText: false,
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.account_circle_outlined,
+                    size: 100,
+                    color: Colors.black,
+                  ),
+                  SizedBox(height: 20),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      side: BorderSide(color: Colors.black, width: 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isLoggedIn = true;
+                      });
+                    },
+                    child: Text(
+                      'Join us Now',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text('to discover more sight of France'),
+                ],
               ),
-              SizedBox(height: 20,),
-              InputTextField(
-                controller: widget.passwordController,
-                hintText: 'Password',
-                obscureText: true,
-              ),
-              SizedBox(height: 10,),
-              Text('Forgot Password?'),
-              SizedBox(height: 20,),
-              SubmitButton(),
-              Spacer(),
-            
-              ],
-              
             ),
-          ),
+            Expanded(
+              child: isLoggedIn == false
+                  ? Container()
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Sign in/up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          child: TextField(
+                            controller: widget.emailController,
+                            decoration: InputDecoration(
+                              hintText: 'email@.com',
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 250, 250, 250),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 211, 211, 211),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 111, 111, 111),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          child: TextField(
+                            controller: widget.passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: '. . . . . .',
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 250, 250, 250),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 211, 211, 211),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 111, 111, 111),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text('Forgot Password?'),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 112, 112, 112),
+                            foregroundColor: Colors.white,
+                            padding: EdgeInsets.all(18),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Sign in/up',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+          ],
         ),
-      );
-    
+      ),
+    );
   }
 }
